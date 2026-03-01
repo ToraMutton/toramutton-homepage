5168a2f67115bdc5c48efc7a1f41192edccf05df
ToraMutton <205386536+ToraMutton@users.noreply.github.com>
2026-03-01 15:45:34 +0900

    fix: justify share button's link

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://toramutton.me',
 	integrations: [mdx(), sitemap()],
 });
