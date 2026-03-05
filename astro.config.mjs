cd6b3ef82fb6858cc6a63fe95a113f6504c2b51c
ToraMutton <205386536+ToraMutton@users.noreply.github.com>
2026-03-05 16:31:47 +0900

    feat: try to add link card

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,14 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+// @ts-ignore
+import remarkLinkCard from 'remark-link-card';
+
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://toramutton.me',
 	integrations: [mdx(), sitemap()],
+	markdown: {
+		remarkPlugins: [remarkLinkCard],
+	},
 });
