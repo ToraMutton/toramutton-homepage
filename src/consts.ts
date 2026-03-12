9e5d800eeb781160470c72b93e186e0fae9b915c
ToraMutton <205386536+ToraMutton@users.noreply.github.com>
2026-03-12 14:01:58 +0900

    feat: add sns link to consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,9 @@
 export const SITE_TITLE = "トラマトのホームページ";
 export const SITE_DESCRIPTION = '日常と技術の、ごちゃまぜ記録';
+
+export const X_DAILY_URL = "https://twitter.com/ToraMutton";
+export const X_TECH_URL = "https://twitter.com/Tr3t_s";
+export const GITHUB_URL = "https://github.com/ToraMutton";
+export const NOTE_URL = "https://note.com/toramutton";
+export const ZENN_URL = "https://zenn.dev/toramutton";
+export const QIITA_URL = "https://qiita.com/ToraMutton";
