c7ecacbe10d1e934ae3b2c97ecfb2b2cc146362e
ToraMutton <205386536+ToraMutton@users.noreply.github.com>
2026-03-12 17:16:27 +0900

    fix: change X to Twitter

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 export const SITE_TITLE = "トラマトのホームページ";
 export const SITE_DESCRIPTION = '日常と技術の、ごちゃまぜ記録';
 
-export const X_DAILY_URL = "https://twitter.com/ToraMutton";
-export const X_TECH_URL = "https://twitter.com/Tr3t_s";
+export const TW_DAILY_URL = "https://twitter.com/ToraMutton";
+export const TW_TECH_URL = "https://twitter.com/Tr3t_s";
 export const GITHUB_URL = "https://github.com/ToraMutton";
 export const NOTE_URL = "https://note.com/toramutton";
 export const ZENN_URL = "https://zenn.dev/toramutton";
